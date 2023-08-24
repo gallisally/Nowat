23b51731e2972c4a71f0e7f69d2684348937d541
Sally Galli <sergiogalli@MacBook-Air-de-Sally.local>
2023-08-24 13:16:24 +0200

    segundo commit

---
+++ credenciales.py
@@ -0,0 +1,8 @@
+CLIENT_KEY= 'KaZwlRwnITu5CCB8ioSTseJZ2nBVXvJvSLiIrlXwIEE'
+CLIENT_SECRET='XZNw7hk6-DkMmCyY81TKAAUa2yCFjSUXcaG54eJZm7M'
+ACCESS_TOKEN='PvGCJPGASd3h4vLIuVId7iAxpXcfiKeNjOb-092RlO4'
+
+#REDIRECT_URI='https://localhost:8080'
+#REDIRECT_URI='urn:ietf:wg:oauth:2.0:oob'
+REDIRECT_URI='https://mastodon.social'
+#REDIRECT_URI='https://mastodon/nowatmat.social'
